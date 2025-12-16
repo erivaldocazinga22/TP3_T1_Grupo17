786671602721b9c88a701339c4f9c0d79a4beea2
erivaldocazinga22 <cazarthdesogner@gmail.com>
2025-12-16 23:52:19 +0100

    feat: add base loop in main.c

--- main.c
+++ main.c
@@ -7,4 +7,35 @@ void limparBuffer() {
     ;
 }
 
-int main() { return 0; }
+int main() {
+  int option;
+
+  do {
+    printf("\n\n-------------------------------------\n\n");
+    printf("\tTAD *PILHA DINAMICAS* \n\n");
+    printf("-------------------------------------\n\n");
+
+    printf("1 - Adicionar Soldado\n");
+    printf("7 - SAIR\n");
+
+    printf("\nEscolha uma Opcao\n");
+
+    if (scanf("%d", &option) != 1) {
+      printf("Entrada inválida!\n");
+      limparBuffer();
+      continue;
+    }
+
+    switch (option) {
+
+    case 7:
+      printf("Saindo...\n");
+      break;
+
+    default:
+      printf("Opcao Invalida!\n");
+    }
+
+  } while (option != 7);
+  return 0;
+}
